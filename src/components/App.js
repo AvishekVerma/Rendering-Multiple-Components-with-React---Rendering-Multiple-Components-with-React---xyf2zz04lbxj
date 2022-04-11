358710494aa93d92bd6c2657aff0a744cc991ca1
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2022-04-11 19:36:49 +0530

    Two new HTML tags added

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test = "project-name">My Portfolio Website</h1>
+      <p data-ns-test = "project-description">Here, I will create my very first Portfolio Website using React.</p>
+    </div>
   )
 }
 
